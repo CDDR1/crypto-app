d048b427413ffcb43f5ca26698a41fbe5415e2ae
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-07-28 18:25:05 -0400

    Adding React Router to the project and configuring routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import NavBar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home"
+import Cryptocurrencies from "./components/Cryptocurrencies"
+import News from "./components/News"
 
 const App = () => {
   return (
     <div className="app">
       <NavBar />
-      <main className="main-content"></main>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+          <Route path="/news" element={<News />} />
+        </Routes>
+      </main>
       <Footer />
     </div>
   );
---
+++ src/components/Cryptocurrencies.jsx
@@ -0,0 +1,5 @@
+const Cryptocurrencies = () => {
+  return <h1>Cryptocurrencies</h1>
+};
+
+export default Cryptocurrencies;
