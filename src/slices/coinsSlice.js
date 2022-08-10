c70e4550c723b60e50069e16668f80c12c95622e
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-08-10 12:52:05 -0500

    Updating coinsSlice code to fetch cryptoApi instead of the JSON placeholder API

--- src/slices/coinsSlice.js
+++ src/slices/coinsSlice.js
@@ -2,10 +2,16 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const cryptoApi = createApi({
   recuderPath: "cryptoApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://jsonplaceholder.typicode.com/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://coinranking1.p.rapidapi.com" }),
   endpoints: (builder) => ({
     getCoins: builder.query({
-      query: () => "/todos",
+      query: () => ({
+        url: "/coins",
+        headers: {
+          'X-RapidAPI-Key': 'fa16bbeba3msh659eb472112419ep1f7f7djsnd35911ebf66e',
+          'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+        }
+      }),
     }),
   }),
 });
