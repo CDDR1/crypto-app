b877ba7ea430746ecaa4be441930282e6a989aa1
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-08-11 15:33:34 -0500

    Adding filtering functionality to cryptocurrencies page

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -2,30 +2,60 @@ import CryptoCard from "./CryptoCard";
 import { Row, Col } from "antd";
 import { useGetCoinsQuery } from "../slices/coinsSlice";
 import { Link } from "react-router-dom";
+import { Typography } from "antd";
+import { Input } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import { useState } from "react";
+
+const { Title } = Typography;
 
 const Cryptocurrencies = ({ simplified }) => {
   const { data } = useGetCoinsQuery();
 
   const coins = simplified ? data.data.coins.slice(0, 10) : data.data.coins;
 
+  const [searchText, setSearchText] = useState("");
+
+  const cryptoCards = coins.map((coin) => 
+    <Col xs={24} md={12} xl={8} xxl={6} key={coin.uuid}>
+      <Link to={`/cryptocurrencies/:${coin.uuid}`}>
+        <CryptoCard 
+          name={coin.name}
+          icon={coin.iconUrl}
+          price={coin.price}
+          marketCap={coin.marketCap}
+          dailyChange={coin.change}
+        />
+      </Link>
+    </Col>
+  );
+
   return (
-    <Row gutter={[32, 32]}>
+    <>
       {
-        coins.map((coin) => 
-          <Col xs={24} md={12} xl={8} xxl={6} key={coin.uuid}>
-            <Link to={`/cryptocurrencies/:${coin.uuid}`}>
-              <CryptoCard 
-                name={coin.name}
-                icon={coin.iconUrl}
-                price={coin.price}
-                marketCap={coin.marketCap}
-                dailyChange={coin.change}
-              />
-            </Link>
-          </Col>
-        )
+        !simplified &&
+        <div className="page-header">
+          <Title level={2}>All Cryptocurrencies</Title>
+          <div className="search-input">
+            <Input 
+              size="large" 
+              placeholder="Searh cryptos..." 
+              prefix={<SearchOutlined />} 
+              className="search-cryptos" 
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+          </div>
+        </div>
       }
-    </Row>
+      <Row gutter={[32, 32]}>
+        {
+          cryptoCards.filter(cryptoCard => {
+            const cryptoCardName = cryptoCard.props.children.props.children.props.name;
+            return cryptoCardName.toLowerCase().includes(searchText.toLowerCase());
+          })
+        }
+      </Row>
+    </>
   );
 };
 
